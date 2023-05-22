e7942520a586ddbf6f63c621d461020bacf4368a
sadmann7 <sadmann898@gmail.com>
2023-05-22 08:47:48 +0600

    debug build issue 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,28 +12,8 @@ interface IndexPageProps {
   }
 }
 
-export default async function IndexPage({ searchParams }: IndexPageProps) {
+export default function IndexPage({ searchParams }: IndexPageProps) {
   const { page, sort, order } = searchParams
 
-  // Current page number
-  const pageNumber = typeof page === "string" ? +page : 1
-  // Number of skaters to show per page
-  const itemsCount = 10
-
-  // Get 10 skaters from the database
-  const skaters = await prisma.skater.findMany({
-    // For server-side pagination
-    take: itemsCount,
-    skip: pageNumber * itemsCount,
-    // For server-side sorting
-    orderBy: {
-      [sort ?? "name"]: order ?? "asc",
-    },
-  })
-
-  return (
-    <main className="container grid items-center py-6">
-      {/* <UnstyledTable data={skaters} itemsCount={itemsCount} /> */}
-    </main>
-  )
+  return <main className="container grid items-center py-6"></main>
 }
